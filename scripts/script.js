11c4c36992c27b9f803acfab6858e40c3536c6a1
Rajveer Singh <singhrajveer4021@gmail.com>
2022-11-16 13:07:53 -0800

    practicing creating and calling functions

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// let name_input = prompt("Enter your name ")
+
+// function user_greeting (user) {
+//     console.log(`Greetings ${name_input}`)
+// }
+
+// user_greeting(name_input)
+
+// let name_input = prompt("Enter your name ")
+
+// const greet = function user_greeting (user, time = "day") {
+//     console.log(`Good ${time} ${user}`);
+
+// }
+
+// greet("Joe", "night");
+
+// greet();
+
+// const greet = (user) => { 
+//     console.log(`Greetings ${user}`);
+// }
+
+// greet(name_input);
